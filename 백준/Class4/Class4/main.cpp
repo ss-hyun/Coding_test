edc1aa63938e652bd5d3d427ccc96e2e9cb0f2eb
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-07-24 17:26:51 +0900

    solve problem 1629

---
+++ 백준/Class4/Class4/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include "class4_problem_list.h"
+
+using namespace std;
+
+int main(){
+
+	cout << "실행시킬 문제 번호 입력 : ";
+
+	int problem;
+	cin >> problem;
+
+	switch (problem){
+	case 1629:
+		cout << problem << "번 문제 솔루션 실행 시작\n";
+		p1629();
+		break;
+	default:
+		cout << problem << "은 없는 문제입니다.\n";
+		break;
+	}
+
+	system("pause");
+
+	return 0;
+}
