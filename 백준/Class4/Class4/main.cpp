3a53edf47357e1c80a0fd54377efe7819c746cc4
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-07-26 21:15:22 +0900

    solve problem 1149

--- 백준/Class4/Class4/main.cpp
+++ 백준/Class4/Class4/main.cpp
@@ -16,6 +16,10 @@ int main(){
 		cout << problem << "번 문제 솔루션 실행 시작\n";
 		p1629();
 		break;
+	case 1149:
+		cout << problem << "번 문제 솔루션 실행 시작\n";
+		p1149();
+		break;
 	default:
 		cout << problem << "은 없는 문제입니다.\n";
 		break;
---
+++ 백준/Class4/Class4/p1149.cpp
@@ -0,0 +1,32 @@
+﻿// 백준 1149번 RGB거리
+#include <iostream>
+
+using namespace std;
+
+#define R 0
+#define G 1
+#define B 2
+
+void p1149(){
+	// min[i] : 0 - r, 1 - g, 2 - b
+	int n, min[2][3], i, r, g, b;
+	bool curr;
+	
+	cin >> n;
+	curr = 0;
+	cin >> min[0][R] >> min[0][G] >> min[0][B];
+	for (i = 1; i < n; i++){
+		curr = !curr;
+		cin >> r >> g >> b;
+		min[curr][R] = r + (min[!curr][G] < min[!curr][B] ? min[!curr][G] : min[!curr][B]);
+		min[curr][G] = g + (min[!curr][R] < min[!curr][B] ? min[!curr][R] : min[!curr][B]);
+		min[curr][B] = b + (min[!curr][R] < min[!curr][G] ? min[!curr][R] : min[!curr][G]);
+	}
+	
+	if (min[curr][R] < min[curr][G])
+		cout << (min[curr][R] < min[curr][B] ? min[curr][R] : min[curr][B]);
+	else
+		cout << (min[curr][G] < min[curr][B] ? min[curr][G] : min[curr][B]);
+
+	return;
+}
