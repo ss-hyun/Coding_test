852dd04b491c9b7e5d51cf1908b6b77dba21eb6d
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-07-30 18:27:23 +0900

    Solve problem 15650
    백준 15650번 N과 M (2) - solved.ac Class 4 에센셜 문제 풀이완료

--- 백준/Class4/Class4/main.cpp
+++ 백준/Class4/Class4/main.cpp
@@ -20,6 +20,10 @@ int main(){
 		cout << problem << "번 문제 솔루션 실행 시작\n";
 		p1149();
 		break;
+	case 15650:
+		cout << problem << "번 문제 솔루션 실행 시작\n";
+		p15650();
+		break;
 	default:
 		cout << problem << "은 없는 문제입니다.\n";
 		break;
---
+++ 백준/Class4/Class4/p15650.cpp
@@ -0,0 +1,29 @@
+﻿// 백준 15650번 N과 M(2)
+#include <iostream>
+
+using namespace std;
+
+void p15650(){
+	int n, m, i, change, limit, seq[8];
+	
+	cin >> n >> m;
+
+	for (i = 0; i < m; i++)
+		seq[i] = i + 1;
+	change = m - 1, limit = n;
+
+	while(true){
+		for (i = 0; i < m; i++)
+			cout << seq[i] << ' ';
+		cout << '\n';
+		if (seq[0] == n - m + 1)
+			break;
+		while (seq[change] == limit)
+			change--, limit--;
+		for (++seq[change++]; change < m; change++)
+			seq[change] = seq[change - 1] + 1;
+		change--, limit = n;
+	}
+
+	return;
+}
