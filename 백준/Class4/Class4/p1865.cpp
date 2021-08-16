9c203ad88d842883ce8da338d444b3bed078e73a
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-08-16 23:53:56 +0900

    Solved problem 1865
    백준 1865번 웜홀
    
       벨만-포드 알고리즘 작성을 완료하여 문제를 해결하였다.

--- 백준/Class4/Class4/p1865.cpp
+++ 백준/Class4/Class4/p1865.cpp
@@ -1,32 +1,38 @@
 ﻿// 백준 1865번 웜홀
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
 
-#define INT_MAX 0x7fffffff
+#define MAX_INIT 0x3fffffff
 
 using namespace std;
 
 namespace s1865{
 	// short_path : 1에서부터 i까지의 short path
-	int n, m, w, undirected_graph[501][501], wormhole[501][501], short_path[501];
-	
+	int n, m, w, short_path[501];
+	vector<pair<int,int>> adj_list[501];
+
 	bool bellman_ford(int start){
-		int i, j, curr, connect;
+		int i, curr;
 		short_path[start] = 0;
 
 		// edge relection n-1번
 		for (i = 1; i < n; i++){
-			for (curr = 1; j < n + 1; j++){
-
-				for (connect = 1; connect < n + 1; connect++){
-					if (undirected_graph[curr][connect] != INT_MAX){
-
-					}
+			for (curr = 1; curr < n + 1; curr++){
+				for (auto connect : adj_list[curr]){
+					if (short_path[connect.first] > short_path[curr] + connect.second)
+						short_path[connect.first] = short_path[curr] + connect.second;
 				}
 			}
 		}
 
 		// all edge에 대해 edge relection 1번
-
+		for (curr = 1; curr < n + 1; curr++){
+			for (auto connect : adj_list[curr]){
+				if (short_path[connect.first] > short_path[curr] + connect.second)
+					return false;
+			}
+		}
 
 		// 정상 종료, negative cycle 없음
 		return true;
@@ -38,36 +44,32 @@ using namespace s1865;
 void p1865(){
 	int tc, i, s, e, t, j;
 
-	cin >> tc;
+	scanf("%d", &tc);
 	while(tc--){
-		cin >> n >> m >> w;
+		scanf("%d%d%d", &n, &m, &w);
 		
 		// 초기화
 		for (i = 1; i < n + 1; i++){
-			for (j = 1; j < n + 1; j++){
-				undirected_graph[i][j] = INT_MAX;
-				wormhole[i][j] = 0;
-			}
-			short_path[i] = INT_MAX;
+			adj_list[i].clear();
+			short_path[i] = MAX_INIT;
 		}
 
 		// 입력 & 그래프 구성
 		for (i = 0; i < m; i++){
-			cin >> s >> e >> t;
-			if (undirected_graph[s][e]>t)
-				undirected_graph[s][e] = t, undirected_graph[e][s] = t;
+			scanf("%d%d%d", &s, &e, &t);
+			adj_list[s].push_back({ e, t });
+			adj_list[e].push_back({ s, t });
 		}
 		for (i = 0; i < w; i++){
-			cin >> s >> e >> t;
-			if (wormhole[s][e]>t)
-				wormhole[s][e] = t;
+			scanf("%d%d%d", &s, &e, &t);
+			adj_list[s].push_back({ e, -t });
 		}
 
 		// 벨만-포드 알고리즘 실행 & 결과 출력
 		if (bellman_ford(1))
-			cout << "YES\n";
+			printf("NO\n");
 		else
-			cout << "NO\n";
+			printf("YES\n");
 	}
 
 	return;
